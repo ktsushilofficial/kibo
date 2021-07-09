e2e4bb6a47e4a2adffd2f25e4058703d9bc1e070
Sushil Tiwari <>
2021-07-09 09:09:26 +0530

    changes

---
+++ cool/target.swift
@@ -0,0 +1,15 @@
+//
+//  target.swift
+//  first pod
+//
+//  Created by Sushil Tiwari on 09/07/21.
+//
+
+import Foundation
+public class  Target
+{
+   public static func printo()
+    {
+    print("this is first pod sushil")
+    }
+}
---
+++ first pod/first_pod.h
@@ -0,0 +1,18 @@
+//
+//  first_pod.h
+//  first pod
+//
+//  Created by Sushil Tiwari on 09/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for first_pod.
+FOUNDATION_EXPORT double first_podVersionNumber;
+
+//! Project version string for first_pod.
+FOUNDATION_EXPORT const unsigned char first_podVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <first_pod/PublicHeader.h>
+
+
